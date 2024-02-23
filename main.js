d877ce080e2c03739364873841868804b5f241bf
Jeyfrem <153380072+dieGeisterhand@users.noreply.github.com>
2024-02-23 08:56:43 -0500

    randombg

--- main.js
+++ main.js
@@ -9,6 +9,14 @@ const randomNumber = Math.floor(Math.random() * imageArray.length);
 
 document.getElementById("main-image").src = imageArray[randomNumber];
 
+function randomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
 
+  const backgroundColor = `rgb(${r}, ${g}, ${b})`;
 
+  document.body.style.background = backgroundColor;
+}
 
+randomColor();
